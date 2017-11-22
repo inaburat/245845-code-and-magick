0f499d3da09c6246e4137fa2e56299353c41167e
Василий Игнатов <inaburat@gmail.com>
2017-11-23 00:17:21 +0300

    task2 fix

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var randomOpacity = function () {
+  var rnd = Math.random();
+  var color = 'rgba(0, 0, 255,' + rnd + ')';
+  return color;
+};
+
 window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
@@ -14,39 +20,22 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
 
-  var max = -1;
-
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
-    }
-  }
-
+  var maxTime = Math.max.apply(null, times);
   var histogramHeight = 150;
-  var step = histogramHeight / max;
+  var step = histogramHeight / maxTime;
   var barWidth = 40;
   var indent = 50 + barWidth;
   var initialX = 155;
   var initialY = 250;
   var lineHeight = 15;
-  var randomOpacity = function () {
-    var rnd = Math.random();
-    var color = 'rgba(0, 0, 255,' + rnd + ')';
-    return color;
-  };
 
-  for (i = 0; i < times.length; i++) {
+  for (var i = 0; i < times.length; i++) {
     ctx.fillStyle = '#000';
     ctx.fillText(parseInt(times[i], 10), initialX + indent * i, initialY - times[i] * step - lineHeight);
 
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = randomOpacity();
-    }
-    ctx.fillRect(initialX + indent * i, initialY - times[i] * step, barWidth, times[i] * step);
+    ctx.fillStyle = (names[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : randomOpacity();
 
+    ctx.fillRect(initialX + indent * i, initialY - times[i] * step, barWidth, times[i] * step);
     ctx.fillStyle = '#000';
     ctx.fillText(names[i], initialX + indent * i, initialY + lineHeight);
   }
